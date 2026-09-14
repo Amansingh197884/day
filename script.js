04e36e9448eba6c0befa50afb6e45afcafae6ae8
Aman Singh <ta5638576@gmail.com>
2026-09-14 16:40:17 +0530

    commit

--- script.js
+++ script.js
@@ -105,9 +105,11 @@ var projectsSwiper = new Swiper(".projectsSwiper", {
   spaceBetween: 18,
   loop: true,
   speed: 600,
+  
   autoplay: {
     delay: 3500,
     disableOnInteraction: false,
+    
   },
   breakpoints: {
     576: {
